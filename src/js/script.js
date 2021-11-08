e30dd7b9aed22a961aff1dce3db258dd91000afc
Vitaliy Kulishov <v.k@gmail.com>
2021-11-08 06:35:26 +0300

    weakend education

--- src/js/script.js
+++ src/js/script.js
@@ -1,60 +1,55 @@
-const slider = tns({
-  container: '.carusel__inner',
-  items: 1,
-  slideBy: 'page',
-  autoplay: false,
-  nav: false,
-  controls: false
-  // controlsText: [
-  //   '<img src="icons/carusel/left-solid.png">', '<img src="icons/carusel/right-solid.png"></img>'
-  // ]
+$(document).ready(function () {
+  const slider = tns({
+    container: '.carusel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    nav: false,
+    controls: false
+  });
+  
+  document.querySelector('.slick-prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  });
+  
+  document.querySelector('.slick-next').addEventListener('click', function () {
+    slider.goTo('next');
+  });
+
+
+$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  // $('.catalog-item__link').each(function (i) {
+  //   $(this).on('click', function(e) {
+  //     e.preventDefault();
+  //     $('.catalog-item__front').eq(i).toggleClass('catalog-item__front_active');
+  //     $('.catalog-item__reverse').eq(i).toggleClass('catalog-item__reverse_active');
+  //   });
+  // });
+  // $('.catalog-item__back').each(function (i) {
+  //   $(this).on('click', function(e) {
+  //     e.preventDefault();
+  //     $('.catalog-item__front').eq(i).toggleClass('catalog-item__front_active');
+  //     $('.catalog-item__reverse').eq(i).toggleClass('catalog-item__reverse_active');
+  //   });
+  // });
+
+
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on('click', function(e) {
+        e.preventDefault();
+        $('.catalog-item__front').eq(i).toggleClass('catalog-item__front_active');
+        $('.catalog-item__reverse').eq(i).toggleClass('catalog-item__reverse_active');
+      });
+    });
+  }
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
 });
 
-document.querySelector('.slick-prev').addEventListener('click', function () {
-  slider.goTo('prev');
-});
 
-document.querySelector('.slick-next').addEventListener('click', function () {
-  slider.goTo('next');
-});
-
-
-
-
-// $(document).ready(function(){
-//     $('.carusel__inner').slick({
-//         speed: 1300,
-//         // adaptiveHeight: true,
-//         autoplay: true,
-//         autoplaySpeed: 2300,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/carusel/left-solid.png"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="icons/carusel/right-solid.png"></button>',
-//         // responsive: [
-//         //     {
-//         //       breakpoint: 768,
-//         //       settings: {
-//         //         dots: true,
-//         //         arrows: false
-
-//         //       }
-//         //     },
-//         //     {
-//         //       breakpoint: 600,
-//         //       settings: {
-//         //         slidesToShow: 2,
-//         //         slidesToScroll: 2
-//         //       }
-//         //     },
-//         //     {
-//         //       breakpoint: 480,
-//         //       settings: {
-//         //         slidesToShow: 1,
-//         //         slidesToScroll: 1
-//         //       }
-//         //     }
-//         //     // You can unslick at a given breakpoint now by adding:
-//         //     // settings: "unslick"
-//         //     // instead of a settings object
-//         //   ]
-//       });
-//   });
